12dd7ba07bd6a6d827f31542e3a40640e64b6f35
amanriyas <153854025+amanriyas@users.noreply.github.com>
2025-02-01 11:34:34 +0400

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Routes,Route } from 'react-router-dom'
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get('http://127.0.0.1:8000/') // Django API endpoint
+      .then((response) => setData(response.data.message)) // Access the `message` key
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <>
+      <div className='flex justify-center items-center h-screen font-bold text-7xl bg-gray-400'>
+        <h1>{data}</h1> 
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ student/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+# Create your views here.
+
+class Index(APIView):
+
+    def get(self,request):
+        return Response({"message": "Hello from Django!"})
+# Create your views here.
