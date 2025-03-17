08b326924833302d472e5b1e45abb98774fd17a0
amanriyas <153854025+amanriyas@users.noreply.github.com>
2025-03-17 17:10:37 +0400

    updated

--- wait_for_db.py
+++ wait_for_db.py
@@ -3,8 +3,8 @@ import psycopg2
 import os
 from dotenv import load_dotenv
 
-# load_dotenv(dotenv_path="/app/.env.development")
-load_dotenv()
+load_dotenv(dotenv_path="/app/.env.development")
+# load_dotenv()
 
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
