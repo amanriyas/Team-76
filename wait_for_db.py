56b3ef3f5067d9a50295644e8151ac56a0ee082b
amanriyas <153854025+amanriyas@users.noreply.github.com>
2025-03-17 17:15:37 +0400

    updated

--- wait_for_db.py
+++ wait_for_db.py
@@ -4,7 +4,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv(dotenv_path="/app/.env.development")
-# load_dotenv()
+load_dotenv()
 
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
